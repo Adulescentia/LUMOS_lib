1266aa8d5d2ee80e1eac1433cebd34a57cf37c9a
Adulescentia <sonsw2422@gmail.com>
2026-05-14 18:31:45 +0900

    8?

--- app/src/main/java/io/github/adulescentia/LUMOS_lib/User.java
+++ app/src/main/java/io/github/adulescentia/LUMOS_lib/User.java
@@ -5,14 +5,8 @@ import org.joml.Vector3f;
 public class User {
     Vector3f userCoordinate;
 
-    User () {
-        this.userCoordinate = new Vector3f(0,0,0);
-    }
-
-    /**/
-    void setUserCoordinate(float wallWidth, float wallLength, float shoulderWidth, Vector3f leftShoulder, Vector3f rightShoulder) {//camera's pos is 0,0, +z behind the camera, +x to the right
-        this.userCoordinate.z = (shoulderWidth*wallLength)/(wallWidth*Math.abs(leftShoulder.sub(rightShoulder).length()));
-        this.userCoordinate.x = (leftShoulder.x + rightShoulder.x)/2;
+    void setUserCoordinate(float wallWidth, float wallLength, float shoulderWidth) {//camera's pos is 0,0, +z behind the camera, +x to the right
+        //todo - 벽 너비와 어깨 너비 비교해 유저 위치 파악 알고리즘 짜기
     }
 
     Vector3f getUserCoordinate() {
