3e278d6e8e4234fe0ade66fb6d43de554c0227d8
Adulescentia <109815556+Adulescentia@users.noreply.github.com>
2026-05-25 22:46:42 +0900

    Add practical debug test app for end-to-end Lumos validation

---
+++ app/src/test/java/io/github/adulescentia/LUMOS_lib/LumosLibraryTest.java
@@ -0,0 +1,32 @@
+package io.github.adulescentia.LUMOS_lib;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class LumosLibraryTest {
+
+    @Test
+    public void registerDeviceAndGetDeviceList() {
+        Lumos lumos = Lumos.getInstance();
+        Device d = lumos.registerDevice(0.0, 1.0, 4.0, "TV", "DISPLAY");
+        assertNotNull(d);
+        assertFalse(lumos.getDeviceList().isEmpty());
+    }
+
+    @Test
+    public void startIoTControlProcessRequiresInitialize() {
+        Lumos lumos = Lumos.getInstance();
+        lumos.initialize();
+        lumos.startIoTControlProcess();
+        assertNotNull(lumos.getLatestResultSnapshot());
+    }
+
+    @Test
+    public void resultProvidesVectors() {
+        Result result = new Result();
+        assertNotNull(result.getDirection());
+        assertNotNull(result.getCurrentPosition());
+        assertNotNull(result.getCameraPos());
+    }
+}
